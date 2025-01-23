5eb84e5f2c9641d15453573cb5d07015a1cb1e40
Akash Pandey <akashpande027@gmail.com>
2025-01-23 20:58:26 +0530

    assignment Level1 23 jan 2025

---
+++ Level1/SumOfNumbers.java
@@ -0,0 +1,22 @@
+package Level1;
+
+import java.util.Scanner;
+public class SumOfNumbers {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int total = 0;
+        int number;
+
+        System.out.println("Enter numbers (enter 0 to stop):");
+
+        do {
+            number = sc.nextInt();
+            total += number;
+        } while (number != 0);
+
+        System.out.println("Sum of entered numbers: " + total);
+
+        sc.close();
+    }
+}
