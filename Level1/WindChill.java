bcae0d95ba2cec9fcd6945b10a1d712dc9fc6dbf
Akash Pandey <akashpande027@gmail.com>
2025-01-28 20:07:06 +0530

    Level1 Assignment

--- Level1/WindChill.java
+++ Level1/WindChill.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 import java.lang.Math;
 class WindChill {
 	public double calculateWindChill(double temperature, double windSpeed) {
-		double windChill = 35.74 + 0.6215 *temp + (0.4275*temp - 35.75) * Math.pow(windSpeed, 0.16);
+		double windChill = 35.74 + 0.6215 *temperature + (0.4275*temperature - 35.75) * Math.pow(windSpeed, 0.16);
 		
 		return windChill;
 	}
@@ -11,6 +11,6 @@ class WindChill {
 		double temp = sc.nextDouble();
 		double windSpeed = sc.nextDouble();
 		WindChill obj = new WindChill();
-		System.out.print("windChill: " + obj.calculateWindChill(temp, windSpeed);
+		System.out.print("windChill: " + obj.calculateWindChill(temp, windSpeed));
 	}
 }
